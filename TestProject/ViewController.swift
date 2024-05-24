2fcab5e2806033f583b8809ab7e4b9e68865a5dc
Alena Vasilenka <emelyanchik.elena@gmail.com>
2024-05-24 13:33:55 +0200

    add simple project for video player

--- TestProject/ViewController.swift
+++ TestProject/ViewController.swift
@@ -6,14 +6,41 @@
 //
 
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        playVideo()
+    }
+    
+    private func findVideo()-> String?{
+        guard let path = Bundle.main.path(forResource: "test", ofType: "mp4") else {
+            debugPrint("test.mp4 not found")
+            return nil
+        }
+        debugPrint("path is \(path)")
+        return path
+    }
+    
+    private func playVideo(){
+        guard let path = findVideo() else {
+            debugPrint("the video was not found")
+            return
+        }
+        
+        let player = AVPlayer(url: URL(fileURLWithPath: path))
+        
+        let playerController = AVPlayerViewController()
+        playerController.player = player
+        present(playerController, animated: true){
+            player.play()
+        }
+    }
 }
 
